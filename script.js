44964a9f6453f1ec4939ac9b8af96c58109c3bec
Chris Twaddell <ct9233@gmail.com>
2020-07-18 19:47:02 -0700

    Next, Prev Song and Progress Bar

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const title = document.getElementById('title');
 const cover = document.getElementById('cover');
 
 // Song titles
-const songs = ['hey', 'summer', 'ukelele'];
+const songs = ['hey', 'summer', 'ukulele'];
 
 // Keep track of song
 let songIndex = 1;
@@ -42,7 +42,47 @@ function pauseSong() {
     audio.pause();
 }
 
+// Previous song
+function prevSong() {
+    songIndex--;
 
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+// Next song
+function nextSong() {
+    songIndex++;
+
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+// Update progress bar
+function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+}
+
+// Set progress bar
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
+}
 
 // Event listeners
 playBtn.addEventListener('click', () => {
@@ -54,3 +94,16 @@ playBtn.addEventListener('click', () => {
         playSong();
     }
 });
+
+// Change song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// Song time update
+audio.addEventListener('timeupdate', updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+// Song ends
+audio.addEventListener('ended', nextSong);
